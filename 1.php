1ea99b32e0eed83c0ef2ad72f9d7f3f5da64eb6c
sejiso <sejiso@alumnatflorida.es>
2025-10-28 11:57:07 +0100

    fina

--- 1.php
+++ 1.php
@@ -1,6 +1,19 @@
 <?php
-//Hola
+$city = "Tokyo,Mexico City,New York City,Mumbai,Seoul,Shanghai,Lagos,Buenos Aires, Cairo,London"; 
+//El ejercicio no te pide un array al principio, sino un string con comas:
+// Esto es solo una cadena de texto, no una lista todavía.
+//Queremos separar cada ciudad por las comas.
+//Para eso usamos la función explode():
+$citiesArray = explode(",", $city); //Crear el array $capitals con la estructura pedida
+//Queremos que cada elemento tenga la forma:
+//['city' => "Tokyo"]
+$capitals = [];
 
+foreach ($citiesArray as $cityName) {
+    $capitals[] = ['city' => trim($cityName)];
+} //Así que hacemos un bucle foreach sobre $citiesArray
+//trim() elimina espacios sobrantes (por ejemplo, antes de “Cairo”).
+var_dump($capitals);
 
 
 ?>
